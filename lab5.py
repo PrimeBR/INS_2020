ce3824d691fc3e8ef9866a84927309da3b9416db
PrimeBR <sdorox@mail.ru>
2020-04-25 17:37:13 +0300

    7381_Dorokh_5

---
+++ lab5.py
@@ -0,0 +1,77 @@
+from keras.datasets import cifar10
+from keras.models import Model
+from keras.layers import Input, MaxPooling2D, Dense, Dropout, Flatten, Convolution2D
+from keras.utils import np_utils
+import numpy as np
+import matplotlib.pyplot as plt
+
+batch_size = 256
+num_epochs = 20
+kernel_size = 5
+pool_size = 2
+conv_depth_1 = 32
+conv_depth_2 = 64
+drop_prob_1 = 0.25
+drop_prob_2 = 0.5
+hidden_size = 512
+
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+num_train, depth, height, width = X_train.shape
+num_test = X_test.shape[0]
+num_classes = np.unique(y_train).shape[0]
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= np.max(X_train)
+X_test /= np.max(X_train)  #
+
+Y_train = np_utils.to_categorical(y_train, num_classes)
+Y_test = np_utils.to_categorical(y_test, num_classes)
+
+
+def run_model(dropout=True):
+    inp = Input(shape=(depth, height, width))
+    conv_1 = Convolution2D(conv_depth_1, (kernel_size, kernel_size),
+                           padding='same', activation='relu')(inp)
+    conv_2 = Convolution2D(conv_depth_1, (kernel_size, kernel_size),
+                           padding='same', activation='relu')(conv_1)
+    pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+    drop_1 = Dropout(drop_prob_1)(pool_1) if dropout else pool_1
+    conv_3 = Convolution2D(conv_depth_2, (kernel_size, kernel_size),
+                           padding='same', activation='relu')(drop_1)
+    conv_4 = Convolution2D(conv_depth_2, (kernel_size, kernel_size),
+                           padding='same', activation='relu')(conv_3)
+    pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
+    drop_2 = Dropout(drop_prob_1)(pool_2) if dropout else pool_2
+    flat = Flatten()(drop_2)
+    hidden = Dense(hidden_size, activation='relu')(flat)
+    drop_3 = Dropout(drop_prob_2)(hidden) if dropout else hidden
+    out = Dense(num_classes, activation='softmax')(drop_3)
+    model = Model(input=inp, output=out)
+    model.compile(loss='categorical_crossentropy',
+
+                  optimizer='adam',
+
+                  metrics=['accuracy'])
+    history = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=num_epochs, verbose=1, validation_split=0.1)
+    model.evaluate(X_test, Y_test, verbose=1)
+
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Training and validation accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Training and validation loos')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+
+run_model()
